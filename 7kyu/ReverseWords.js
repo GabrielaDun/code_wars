a64364f48630145fb057efe2d961c62dbc954166
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-09-10 14:28:55 +0200

    Reverse Wrods

---
+++ 7kyu/ReverseWords.js
@@ -0,0 +1,23 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// My solution:
+
+function reverseWords(str) {
+    return str
+      .split(' ')
+      .map(letters => letters.split('').reverse().join(''))
+      .join(' ');
+  }
+
+  // Other good solutions:
+
+function reverseWords(str) {
+    return str
+        .split("").reverse().join("").
+        split(" ").reverse().join(" ");
+}
+  
